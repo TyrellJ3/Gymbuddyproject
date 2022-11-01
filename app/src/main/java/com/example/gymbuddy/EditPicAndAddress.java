28827616b39180e425cc346593a44318fb0de8c2
ALEXECKERT93 <78118345+ALEXECKERT93@users.noreply.github.com>
2022-10-31 20:22:27 -0400

    got pic to store temporarily

--- app/src/main/java/com/example/gymbuddy/EditPicAndAddress.java
+++ app/src/main/java/com/example/gymbuddy/EditPicAndAddress.java
@@ -14,10 +14,15 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -36,15 +41,17 @@ public class EditPicAndAddress extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_pic_address);
         ImageView myImg =(ImageView)findViewById(R.id.myPic);
-
+        final int PICK_IMAGE = 100;
 
 
 
             myImg.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                   
 
+                    Intent gallery = new Intent(Intent.ACTION_PICK,
+                            MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                    startActivityForResult(gallery, PICK_IMAGE);
 
                 }
 
@@ -52,4 +59,18 @@ public class EditPicAndAddress extends AppCompatActivity {
             });
 
 
-    }}
+    }
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        Uri imageUri;
+        ImageView imageView = findViewById(R.id.myPic);
+
+        if (resultCode == RESULT_OK && reqCode == 100){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
+    }
+
+}
+
